0ddab2abc2c6014d4c571ca8dc70eb97480807ce
Smokie <29041430+smokes@users.noreply.github.com>
2019-03-13 12:54:37 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+const dotenv = require("dotenv").config({
+	path: "../.env"
+});
+const path = require("path");
+const slash = require("slash");
+const watcher = require("./util/watcher")
+
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+const categories = fs.readdirSync("./commands");
+
+client.commands = new Discord.Collection();
+client.directories = new Discord.Collection();
+client.categories = new Discord.Collection();
+
+categoriesDirectories = categories.map(categoryName =>
+	slash(path.join("./commands", categoryName))
+);
+
+categoriesDirectories.forEach(category => {
+	let commands = fs.readdirSync(category);
+
+	let commandsName = commands.map((command) => command.split(".")[0]);
+	let categoryName = category.split("/")[1]
+	client.categories.set(categoryName, commandsName);
+
+
+	commands.forEach((command) => {
+		if (!command.endsWith(".js")) return;
+		let commandName = command.split(".")[0];
+		let commandPath = slash(path.join(category, commandName));
+		const commandProp = require("./" + commandPath);
+		client.commands.set(commandName, commandProp)
+		client.directories.set(commandName, commandPath);
+	})
+});
+
+const events = fs.readdirSync("./events");
+
+events.forEach((event) => {
+	if (!event.endsWith(".js")) return;
+	let eventName = event.split(".")[0];
+	let eventPath = slash(path.join("./events", eventName));
+	const eventProp = require("./" + eventPath);
+	client.on(eventName, (...args) => eventProp.run(client, ...args));
+})
+
+watcher(client, process.env.NODE_ENV);
+
+client.login(process.env.DISCORD_TOKEN);
